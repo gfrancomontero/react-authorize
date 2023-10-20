667dc0ece2ad1313c0315d48001a56affd4279af
Gonzalo Franco <65114161+gfrancomontero@users.noreply.github.com>
2023-10-20 17:08:20 -0400

    first commit

---
+++ src/utils/requests.ts
@@ -0,0 +1,68 @@
+import {
+  PaginatedRequestParams,
+  PaginatedResponse,
+  RequestByEmployeeParams,
+  SetTransactionApprovalParams,
+  Transaction,
+  Employee,
+} from "./types"
+import mockData from "../mock-data.json"
+
+const TRANSACTIONS_PER_PAGE = 5
+// bug 4/7 - fixed i need to know how many transactions there are in total
+// dynamically, so I can add those to the transactions per page number
+const ALL_TRANSACTIONS = mockData["transactions"].length
+
+const data: { employees: Employee[]; transactions: Transaction[] } = {
+  employees: mockData.employees,
+  transactions: mockData.transactions,
+}
+
+export const getEmployees = (): Employee[] => data.employees
+
+export const getTransactionsPaginated = ({
+  page,
+}: PaginatedRequestParams): PaginatedResponse<Transaction[]> => {
+  if (page === null) {
+    throw new Error("Page cannot be null")
+  }
+
+  const start = page * TRANSACTIONS_PER_PAGE
+
+  // bug 4/7 fixed
+  // const end = start + TRANSACTIONS_PER_PAGE
+  const end = ALL_TRANSACTIONS
+
+  if (start > data.transactions.length) {
+    throw new Error(`Invalid page ${page}`)
+  }
+
+  const nextPage = end < data.transactions.length ? page + 1 : null
+
+  return {
+    nextPage,
+    data: data.transactions.slice(start, end),
+  }
+}
+
+export const getTransactionsByEmployee = ({ employeeId }: RequestByEmployeeParams) => {
+  if (!employeeId) {
+    // throw new Error("Employee id cannot be empty")
+    // Bug 3/7 fixed - if there is no employeeId, we return all of the transactions.
+    return data.transactions
+  } else {
+    return data.transactions.filter((transaction) => transaction.employee.id === employeeId)
+  }
+}
+
+export const setTransactionApproval = ({ transactionId, value }: SetTransactionApprovalParams): void => {
+  const transaction = data.transactions.find(
+    (currentTransaction) => currentTransaction.id === transactionId
+  )
+
+  if (!transaction) {
+    throw new Error("Invalid transaction to approve")
+  }
+
+  transaction.approved = value
+}
